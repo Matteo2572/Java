6245b2a993931eb8a8bc393873ea3a854ebbe0b8
Matteo2572 <imbastaromatteo@gmail>
2023-02-24 16:22:38 +0100

    lab

---
+++ Lezione1Lab/Calcolatrice.java
@@ -0,0 +1,80 @@
+
+package Lezione1Lab;
+
+import java.util.Scanner;
+
+/**
+ * @author Io
+ *         In questa classe implemento le funzionalità di una calcolatrice
+ *         classica
+ *         fondamentali:
+ *         1: addizione
+ *         2: sottrazione
+ *         3: prodotto
+ *         4: divisione
+ */
+public class Calcolatrice {
+    public static void main(String[] args) {
+        Scanner tastiera = new Scanner(System.in);
+
+        String domanda = "Inserisci n1";
+        System.out.println(domanda);
+        int n1 = tastiera.nextInt(); // primoNumero
+
+        String domanda2 = "Inserisci n2";
+        System.out.println(domanda2);
+        int n2 = tastiera.nextInt(); // secondoNumero
+
+        /* Sezione Somma */
+        int rSomm = n1 + n2; // risultatoSomma
+        System.out.println("Il risultato della somma é: " + rSomm); // stampa della somma
+
+        /* Sezione Differenza */
+        int rDiff = n1 - n2; // risultatoDifferenza
+        System.out.println("Il risultato della differenza é: " + rDiff); // stampa della differenza
+
+        /* Sezione Prodotto */
+        int rProd;
+        if (n1 == 0 || n2 == 0) {
+            rProd = 0;
+        } else {
+            rProd = n1 * n2; // risultatoProdotto
+        }
+        System.out.println("Il risultato del prodotto è: " + rProd); // stampa del prodotto
+
+        /* Sezione Divisione */
+        float rDiv;
+        if (n2 == 0) {
+            System.out.println("Il risultato della divisione è: Errore, impossibile eseguire la divisione");
+        } else if (n1 == 0) {
+            rDiv = 0; // risultatoDivisione
+            System.out.println("Il risultato della divisione è: " + rDiv); // stampa della divisione
+        } else {
+            rDiv = n1 / (float) n2;
+            System.out.println("Il risultato della divisione è: " + rDiv); // stampa della divisione
+        }
+
+        /* Sezione Seno n1 */
+        double rSenoN1;
+        double n1Rad = Math.toRadians(n1);
+        rSenoN1 = Math.sin(n1Rad);
+        System.out.println("Il risultato del seno di n1 è: " + rSenoN1+" Radiante: "+n1Rad); // stampa del seno di n1
+
+        /* Sezione Seno n2 */
+        double rSenoN2;
+        double n2Rad = Math.toRadians(n2);
+        rSenoN2 = Math.sin(n2Rad);
+        System.out.println("Il risultato del seno di n2 è: " + rSenoN2 +" Radiante: "+n2Rad); // stampa del seno di n2
+
+        /* Sezione Coseno n1 */
+        double rCosenoN1;
+        rCosenoN1 = Math.sin(n1Rad);
+        System.out.println("Il risultato del coseno di n1 è: " + rCosenoN1 +" Radiante: "+n1Rad); // stampa del coseno di n1
+
+        /* Sezione Coeno n2 */
+        double rCosenoN2;
+        rCosenoN2 = Math.sin(n2Rad);
+        System.out.println("Il risultato del coseno di n2 è: " + rCosenoN2 +" Radiante: "+n2Rad); // stampa del coseno di n2
+
+    } // end main
+} // end class
--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # Java
 Corso Engim Java
+
+1. capire la tipologia
+    -scientifica
+    -classica  --> interi --> operazioni a 2 cifre --> almeno due var --> 4op 
+    [addizione--> i: primoNumero int, secondoNumero int / o:risulatoSomma int / io: risultatoSomma]
+    [differenza--> i: primoNumero int, secondoNumero int / o:risulatoDifferenza int / io: risulatoDifferenza]
+    [prodotto--> i: primoNumero int, secondoNumero int / o:risulatoProdotto int / io: risulatoProdotto]
+    [divisione--> i: primoNumero int, secondoNumero int(cast in double o float) / o:risulatoDivisione (double o float) / io: risulatoDivisione]
+
+2. tipologia di dati accettati
+    -numerici interi
+    -numerici virgola
+
+3. Pseudocodice
+    -dichiaro variabili input
+    -assegnazione/valorizzazione 
+        pN=8 sN=10 
+        rSomm=pN+sN 
+        rDiff=pN-sN 
+        rProd=pN*sN 
+        rDiv=pN/sN
+
+
