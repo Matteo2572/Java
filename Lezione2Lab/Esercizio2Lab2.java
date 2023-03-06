f46a2c01a32e3c54372b43f0492311ab995ec8a0
Matteo2572 <imbastaromatteo@gmail>
2023-03-06 11:55:38 +0100

    aggiornamento

--- Lezione2Lab/Esercizio2Lab2.java
+++ Lezione2Lab/Esercizio2Lab2.java
@@ -5,16 +5,16 @@ public class Esercizio2Lab2 {
 
         //tipoDati[] nomeArray = new tipoDati[dimensione];
         int[] numeri = {1, 2, 3, 4, 5};
-
-        public static double calcolaMedia(int[] numeri) {
-            double somma = 0;
-            for (int i = 0; i < numeri.length; i++) {
-                somma += numeri[i];
-            }
-            double media = somma / numeri.length;
-            return media;
-        }
         double media = calcolaMedia(numeri);
         System.out.println("La media dei numeri è " + media);
+    }
+
+    public static double calcolaMedia(int[] numeri) {
+        double somma = 0;
+        for (int i = 0; i < numeri.length; i++) {
+            somma += numeri[i];
+        }
+        double media = somma / numeri.length;
+        return media;
     }
 }
